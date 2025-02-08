767e49e5f2837975f6962dc8bd09a0aef96f4911
tushardev09 <tusharok88@gmail.com>
2025-02-08 04:08:05 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,12 +62,12 @@ API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ACCESS_SECRET = os.getenv("ACCESS_SECRET")
-BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+
 
 
 # Authenticate with Twitter using Tweepy
 lient = tweepy.OAuth1UserHandler(
-    bearer_token=BEARER_TOKEN,  # Use Bearer Token for v2 authentication
+  # Use Bearer Token for v2 authentication
     consumer_key=API_KEY,
     consumer_secret=API_SECRET,
     access_token=ACCESS_TOKEN,
