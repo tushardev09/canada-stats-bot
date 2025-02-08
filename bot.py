7d8ea678a31613730849bb05f1394c4175aef670
tushardev09 <tusharok88@gmail.com>
2025-02-08 02:11:29 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ACCESS_SECRET = os.getenv("ACCESS_SECRET")
-
+BEARER_TOKEN = os.getenv("ACCESS_BEARER")
 # Authenticate with Twitter
 auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
